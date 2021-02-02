31e8ee074db2d008ff7766b53d1603cce75b4c07
Lesia-f <73548746+Lesia-f@users.noreply.github.com>
2021-02-02 10:23:37 +0200

    heder

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,15 +1,13 @@
-  
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+(() => {  const menuBtnRef = document.querySelector("[data-menu-button]");
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+const mobileMenuRef = document.querySelector("[data-menu]");
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+menuBtnRef.addEventListener("click", () => {    const expanded =
 
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+menuBtnRef.classList.toggle("is-open");
+
+menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+mobileMenuRef.classList.toggle("is-open");  });})();
